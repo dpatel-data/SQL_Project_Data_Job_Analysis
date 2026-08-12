7c8a2d730933e2fa192d4aa2d44e65649c99bea7
Dhruv <dpatel.data@gmail.com>
2026-08-12 17:36:29 -0400

    identifying the top paying jobs in the dataset.

---
+++ project_sql/1_top_paying_jobs.sql
@@ -0,0 +1,91 @@
+-- QUESTION: What are the top-paying data analyst jobs?
+
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+from
+    job_postings_fact
+
+
+
+-- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+from
+    job_postings_fact
+where
+    job_title_short = 'Data Analyst' and
+    job_location = 'Anywhere'
+
+
+
+-- Focuse on job postings with specified salaries (remove nulls).
+
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+from
+    job_postings_fact
+where
+    job_title_short = 'Data Analyst' and
+    job_location = 'Anywhere' and
+    salary_year_avg is not null
+
+
+
+-- Why? Hilight the top-paying opportunities for Data Analysts...
+
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+from
+    job_postings_fact
+where
+    job_title_short = 'Data Analyst' and
+    job_location = 'Anywhere' and
+    salary_year_avg is not null
+order by
+    salary_year_avg desc
+limit 10
+
+
+
+-- Joining tables to add companies...
+
+select
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name as company_name
+from
+    job_postings_fact
+left join
+    company_dim on job_postings_fact.company_id = company_dim.company_id
+where
+    job_title_short = 'Data Analyst' and
+    job_location = 'Anywhere' and
+    salary_year_avg is not null
+order by
+    salary_year_avg desc
+limit 10
